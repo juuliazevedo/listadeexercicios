eb7d377d1f094b21b9e9a58937c710ae334f5529
juuliazevedo <lmariajulia445@gmail.com>
2025-08-12 22:28:06 -0300

    Solução concluída do 'Invertendo a Lista'

---
+++ 15invertendo_a_lista.py
@@ -0,0 +1,8 @@
+# Invertendo a Lista - 15
+
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+def inverte_lista(lista):
+  print(lista[::-1])
+
+inverte_lista(numeros)
