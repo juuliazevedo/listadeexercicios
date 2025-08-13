08c9387301e7b5242ca666015f48e4b36556a36a
juuliazevedo <lmariajulia445@gmail.com>
2025-08-12 22:27:58 -0300

    Solução concluída do 'Percorrendo uma Lista'

---
+++ 1percorrendo_uma_lista.py
@@ -0,0 +1,9 @@
+# Percorrendo uma Lista - 1
+
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+def percorrer(lista):
+  for posição in lista:
+    print(posição)
+
+percorrer(numeros)
