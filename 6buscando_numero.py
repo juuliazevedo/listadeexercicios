ae3176c2a23e228a3afdaf70d9e736ec64359b37
juuliazevedo <lmariajulia445@gmail.com>
2025-08-12 22:28:01 -0300

    Solução concluída do 'Buscando um Número'

---
+++ 6buscando_numero.py
@@ -0,0 +1,12 @@
+# Buscando um Número 6
+
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+num = int(input())
+
+def busca_de_numero(lista, numero):
+    if numero in lista:
+      return True
+    else:
+      return False
+
+print(busca_de_numero(numeros,num))
